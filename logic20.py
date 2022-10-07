5dc8079a4655861d10bb4e356510173ae41cbf3c
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2022-10-07 11:17:36 +0500

    logic20.py

--- logic20.py
+++ logic20.py
@@ -8,5 +8,30 @@ def main(n):
     Returns:
         bool: answer
     """
+    number0=0
+    number1=0
 
-    return
+    x1=n%10
+    number0+=x1==0
+    number1+=x1==1
+    n//=10
+
+    x2=n%10
+    number0+=x2==0 and n !=0
+    number1+=x2==1
+    n//=10
+
+    x3=n%10
+    number0+=x3==0 and n !=0
+    number1+=x3==1
+    n//=10
+
+    x4=n%10
+    number0+=x4==0 and n !=0
+    number1+=x4==1
+    n//=10
+
+    return number1>number0
+
+print(main(1111))
+
